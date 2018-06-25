4ba05cb69ebc2fb74e255fe82dd15e8d3b5e6621
chrishu <tcpdumpp@163.com>
2018-06-25 15:51:02 +0800

    add native invoke api

---
+++ Neo.SmartContract.Framework/Services/Neo/Native.cs
@@ -0,0 +1,10 @@
+﻿namespace Neo.SmartContract.Framework.Services.Neo
+{
+    public class Native
+    {
+
+        [Syscall("Ontology.Native.Invoke")]
+        public static extern object Invoke(int version, byte[] address, byte[] method, object[] args);
+
+    }
+}
